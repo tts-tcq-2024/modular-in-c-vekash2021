87a4ee80d3a2aa42c3c771b92af906fde4620cbe
vekash2021 <106808639+vekash2021@users.noreply.github.com>
2024-07-28 12:51:37 +0530

    Update ColorPairToNumConv.h

--- ColorPairToNumConv.h
+++ ColorPairToNumConv.h
@@ -1,4 +1,8 @@
+#ifndef COLORPAIRTONUMCONV_H
+#define COLORPAIRTONUMCONV_H
+
 int GetNumberFrompairColor(const ColorPair* colorPair) {
     return colorPair->majorColor * numberOfMinorColors +
             colorPair->minorColor + 1;
 }
+#endif
